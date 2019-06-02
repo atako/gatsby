51177dc0f013d8019efe97953a67dfdc23c58bb0
Dmitry Ryumkin <riumkin@gmail.com>
2019-06-02 19:41:34 +1000

    add link and about

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {Link} from "gatsby"
+
+export default () => (
+  <>
+    <h1>About me</h1>
+    <p><Link to="/">Main</Link></p>
+  </>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import {Link} from "gatsby"
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+<h1>Hello Frontend Masters!</h1>
+<p><Link to="/about">About</Link></p>
+</>
+)
+
