56501b491d08e9bbbf3eba88e7380b0ac37cfa16
Dmitry Ryumkin <riumkin@gmail.com>
2019-06-02 20:31:57 +1000

    add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from "gatsby"
+import Layout from "../components/layout"
 
 export default () => (
-  <>
+  <Layout>
 <h1>Hello Frontend Masters!</h1>
 <p><Link to="/about">About</Link></p>
-</>
+</Layout>
 )
 
